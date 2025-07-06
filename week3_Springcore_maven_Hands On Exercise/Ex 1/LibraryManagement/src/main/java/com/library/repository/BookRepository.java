fd75aedc64601383b86ef17f932a627b7a2f7e7b
Roshan Rahul Ram C <153794219+Roshanrahulram@users.noreply.github.com>
2025-07-06 22:24:57 +0530

    Add files via upload

---
+++ week3_Springcore_maven_Hands On Exercise/Ex 1/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+	public void bookRepositoryShow()
+	{
+		System.out.println("This is the  BookRepository");
+	}
+}
---
+++ week3_Springcore_maven_Hands On Exercise/Ex 1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+	public void bookServiceShow()
+	{
+		System.out.println("This is the BookService");
+	}
+}
